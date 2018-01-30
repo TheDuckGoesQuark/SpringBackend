5a274562199d645d993c06be25962ecf979124ff
Jordan Mackie <jm354@st-andrews.ac.uk>
2018-01-30 14:52:33 +0000

    Added project list model to user model.

--- src/main/java/BE/responsemodels/UserModel.java
+++ src/main/java/BE/responsemodels/UserModel.java
@@ -1,9 +1,41 @@
 package BE.responsemodels;
 
+import java.util.List;
+
 public class UserModel {
+
+    class ProjectListModel {
+        private String project_name;
+        private String access_level;
+
+        public ProjectListModel(String project_name, String access_level) {
+            this.project_name = project_name;
+            this.access_level = access_level;
+        }
+
+        public String getProject_name() {
+            return project_name;
+        }
+
+        public void setProject_name(String project_name) {
+            this.project_name = project_name;
+        }
+
+        public String getAccess_level() {
+            return access_level;
+        }
+
+        public void setAccess_level(String access_level) {
+            this.access_level = access_level;
+        }
+    }
+
     private String username;
 
     private String password;
 
     private String email;
+
+    private List<ProjectListModel> projects;
+
 }
