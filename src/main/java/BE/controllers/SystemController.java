a8624353eff54609e6ecc649e2a558895cf91d66
Jordan Mackie <jm354@st-andrews.ac.uk>
2018-02-01 18:11:44 +0000

    Had non-compiling version in dev. Fixed.

--- src/main/java/BE/controllers/SystemController.java
+++ src/main/java/BE/controllers/SystemController.java
@@ -1,14 +1,14 @@
-package BE.controllers;
-
-import BE.entities.user.User;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-public class SystemController {
-
-    @RequestMapping(value = "/users/{username}", method = RequestMethod.POST)
-    public User createUser(@PathVariable(value="username") String username, @RequestBody User user) {
-        return userService.createUser(user);
-    }
-
-}
+//package BE.controllers;
+//
+//import BE.entities.user.User;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//public class SystemController {
+//
+//    @RequestMapping(value = "/users/{username}", method = RequestMethod.POST)
+//    public User createUser(@PathVariable(value="username") String username, @RequestBody User user) {
+//        return userService.createUser(user);
+//    }
+//
+//}
