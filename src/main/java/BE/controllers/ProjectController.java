23adcc03c120364bde656233a649dc74f6d490d6
nd33 <nd33@st-andrews.ac.uk>
2018-02-01 21:57:33 +0000

    Separating file controller methods from project controller, because having them in the same class brakes the files part.

--- src/main/java/BE/controllers/ProjectController.java
+++ src/main/java/BE/controllers/ProjectController.java
@@ -22,9 +22,6 @@ public class ProjectController {
     @Autowired
     ProjectService projectService;
 
-    @Autowired
-    FileService fileService;
-
     /**
      * Gets all projects
      * @return a list of all projects
@@ -77,63 +74,5 @@ public class ProjectController {
         //TODO this
         throw new NotImplementedException();
     }
-
-
-    /**
-     * Gets all files of a project
-     * @return a list of all projects
-     **/
-    @RequestMapping(value = "/projects/{project_name}/files", method = RequestMethod.GET)
-    public List<File> getAllFiles(@PathVariable(value="project_name") String project_name) {
-        return fileService.getAllFiles(project_name);
-    }
-
-    /**
-     * @param project_name
-     * @return a particular file
-     */
-    @RequestMapping(value = "/project/{project_name}/**", method = RequestMethod.GET)
-    public File getFile(@PathVariable(value="project_name") String project_name,
-                        HttpServletRequest request) {
-        String path  = (String) request.getAttribute(
-                HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
-        return fileService.getFile(project_name, path);
-    }
-
-    /**
-     * @return
-     */
-    @RequestMapping(value = "/project/{project_name}/**", method = RequestMethod.POST)
-    public File createFile(@PathVariable(value="project_name") String file_name,
-                           HttpServletRequest request) {
-        String path  = (String) request.getAttribute(
-                HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
-        return fileService.createFile(file_name, path);
-    }
-//
-//    /**
-//     * @param project_name
-//     * @param project
-//     * @return
-//     */
-//    @RequestMapping(value = "/projects/{project_name}", method = RequestMethod.PATCH)
-//    public Project updateProject(@PathVariable(value="project_name") String project_name, @RequestBody Project project) {
-//        return projectService.updateProject(project);
-//    }
-//
-//    /**
-//     * @param project_name
-//     * @return
-//     */
-//    @RequestMapping(value = "/project/{project_name}", method = RequestMethod.DELETE)
-//    public Project deleteProject(@PathVariable(value="project_name") String project_name) {
-//        return projectService.deleteProject(project_name);
-//    }
-//
-//    @RequestMapping(value = "/project_roles", method = RequestMethod.GET)
-//    public Role getProjectRoles() throws NotImplementedException {
-//        //TODO this
-//        throw new NotImplementedException();
-//    }
 
 }
