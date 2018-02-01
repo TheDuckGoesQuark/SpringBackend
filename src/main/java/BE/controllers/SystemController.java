fa025f8c2605605e35b64c5042a15592fe73937c
Jordan Mackie <jm354@st-andrews.ac.uk>
2018-02-01 17:04:23 +0000

    Beginning implementation of system controller/service for logs and other information on feature branch.

---
+++ src/main/java/BE/controllers/SystemController.java
@@ -0,0 +1,15 @@
+package BE.controllers;
+
+import BE.entities.project.File;
+import BE.services.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+public class SystemController {
+    
+}
