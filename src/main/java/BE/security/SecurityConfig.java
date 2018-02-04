b83a24220b82cfb12ba96b3b563e9961ac52719b
Jordan Mackie <jm354@st-andrews.ac.uk>
2018-02-04 16:32:33 +0000

    Working on allowing access to token controller

--- src/main/java/BE/security/SecurityConfig.java
+++ src/main/java/BE/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .antMatcher("/oauth/token")
                 .authorizeRequests()
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
