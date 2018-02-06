ac04ceb15237110e0451199a3cd2c46d7a76a117
td38 <td38@st-andrews.ac.uk>
2018-02-06 15:33:34 +0000

    Added controller for uploading files

--- src/main/java/BE/controllers/ProjectController.java
+++ src/main/java/BE/controllers/ProjectController.java
@@ -1,17 +1,30 @@
 package BE.controllers;
 
+// JavaIO
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
-
 // Exceptions
 import BE.exceptions.NotImplementedException;
+import java.io.IOException;
 // Models
 import BE.models.project.ProjectModel;
 import BE.models.project.RoleModel;
 import BE.models.user.UserModel;
-// Spring
+// Apache & Spring
+import org.apache.tomcat.util.http.fileupload.FileItemIterator;
+import org.apache.tomcat.util.http.fileupload.FileItemStream;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class ProjectController {
@@ -41,6 +54,47 @@ public class ProjectController {
     public RoleModel getProjectRoles() throws NotImplementedException {
         //TODO this
         throw new NotImplementedException();
+    }
+
+    @RequestMapping(value="/upload", method= RequestMethod.POST)
+    public void upload(HttpServletRequest request) {
+        try {
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+        if (!isMultipart) {
+            // Inform user about invalid request
+            System.out.println("Invalid request");
+            return;
+        }
+
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload();
+
+        // Parse the request
+            FileItemIterator iterator = upload.getItemIterator(request);
+            while (iterator.hasNext()) {
+                FileItemStream item = iterator.next();
+                String name = item.getFieldName();
+                InputStream stream = item.openStream();
+                if (!item.isFormField()) {
+                    String filename = item.getName();
+                    // Process the input stream
+                    System.out.println("File detected");
+                    OutputStream out = new FileOutputStream(filename);
+                    IOUtils.copy(stream, out);
+                    stream.close();
+                    out.close();
+                }
+            }
+        }catch (FileUploadException | IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping(value = "/uploader", method = RequestMethod.GET)
+    public ModelAndView uploaderPage() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("uploader");
+        return model;
     }
 
 }
